afd6619826ed2a0d8db6bbdf82538dac2b346ce9
Lassassin <sanjid8426@gmail.com>
2021-02-15 13:48:08 +0600

    Simplified stuff

--- app/src/main/java/com/example/jobseeker/app/homePage/CreateJob.java
+++ app/src/main/java/com/example/jobseeker/app/homePage/CreateJob.java
@@ -1,21 +1,22 @@
 package com.example.jobseeker.app.homePage;
 
 import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
 
-public class CreateJob extends WearableActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private TextView mTextView;
+import com.example.jobseeker.R;
+import com.example.jobseeker.databinding.ActivityCreateJobBinding;
 
+public class CreateJob extends AppCompatActivity {
+
+    ActivityCreateJobBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityCreateJobBinding.inflate(getLayoutInflater());
+
         setContentView(R.layout.activity_create_job);
 
-        mTextView = (TextView) findViewById(R.id.text);
-
-        // Enables Always-on
-        setAmbientEnabled();
     }
 }
