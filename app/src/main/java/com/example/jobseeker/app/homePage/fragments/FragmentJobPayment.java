92a926f06bbf3eda093c80bc562e3489dc8b2b6b
AhmedSaadTanim <astdragneel@yahoo.com>
2021-02-28 15:22:16 +0600

    Files

--- app/src/main/java/com/example/jobseeker/app/homePage/fragments/FragmentJobPayment.java
+++ app/src/main/java/com/example/jobseeker/app/homePage/fragments/FragmentJobPayment.java
@@ -1,6 +1,10 @@
 package com.example.jobseeker.app.homePage.fragments;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,18 +16,129 @@ import androidx.fragment.app.Fragment;
 import com.example.jobseeker.R;
 import com.example.jobseeker.databinding.FragmentCreateJobPaymentBinding;
 
+import java.io.File;
+
+import static android.app.Activity.RESULT_OK;
+
 public class FragmentJobPayment extends Fragment {
     FragmentCreateJobPaymentBinding binding;
+    Intent myFileIntent;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return  (binding = FragmentCreateJobPaymentBinding.inflate(inflater, container, false)).getRoot();
+        return (binding = FragmentCreateJobPaymentBinding.inflate(inflater, container, false)).getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        binding.addFile1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                myFileIntent.setType("*/*");
+                startActivityForResult(myFileIntent, 1);
+            }
+        });
+        binding.addFile2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                myFileIntent.setType("*/*");
+                startActivityForResult(myFileIntent, 2);
+            }
+        });
+        binding.addFile3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                myFileIntent.setType("*/*");
+                startActivityForResult(myFileIntent, 3);
+            }
+        });
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    Uri uri = data.getData();
+                    String uriString = uri.toString();
+                    File myFile = new File(uriString);
+                    String path = myFile.getAbsolutePath();
+                    String displayName = null;
+
+                    if (uriString.startsWith("content://")) {
+                        Cursor cursor = null;
+                        try {
+                            cursor = getActivity().getContentResolver().query(uri, null, null, null, null);
+                            if (cursor != null && cursor.moveToFirst()) {
+                                displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                            }
+                        } finally {
+                            cursor.close();
+                        }
+                    } else if (uriString.startsWith("file://")) {
+                        displayName = myFile.getName();
+                    }
+
+                    binding.file1.setText(displayName);
+                }
+                break;
+            case 2:
+                if (resultCode == RESULT_OK) {
+                    Uri uri = data.getData();
+                    String uriString = uri.toString();
+                    File myFile = new File(uriString);
+                    String path = myFile.getAbsolutePath();
+                    String displayName = null;
+
+                    if (uriString.startsWith("content://")) {
+                        Cursor cursor = null;
+                        try {
+                            cursor = getActivity().getContentResolver().query(uri, null, null, null, null);
+                            if (cursor != null && cursor.moveToFirst()) {
+                                displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                            }
+                        } finally {
+                            cursor.close();
+                        }
+                    } else if (uriString.startsWith("file://")) {
+                        displayName = myFile.getName();
+                    }
+
+                    binding.file2.setText(displayName);
+                }
+                break;
+            case 3:
+                if (resultCode == RESULT_OK) {
+                    Uri uri = data.getData();
+                    String uriString = uri.toString();
+                    File myFile = new File(uriString);
+                    String path = myFile.getAbsolutePath();
+                    String displayName = null;
+
+                    if (uriString.startsWith("content://")) {
+                        Cursor cursor = null;
+                        try {
+                            cursor = getActivity().getContentResolver().query(uri, null, null, null, null);
+                            if (cursor != null && cursor.moveToFirst()) {
+                                displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                            }
+                        } finally {
+                            cursor.close();
+                        }
+                    } else if (uriString.startsWith("file://")) {
+                        displayName = myFile.getName();
+                    }
+
+                    binding.file3.setText(displayName);
+                }
+                break;
+        }
     }
 }
