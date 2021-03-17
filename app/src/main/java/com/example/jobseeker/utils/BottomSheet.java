18099b4847f1e54188c48b5d2c2947e0f85c6559
AtiqurRahmanAni <56642339+AtiqurRahmanAni@users.noreply.github.com>
2021-03-17 16:14:48 +0600

    gg

--- app/src/main/java/com/example/jobseeker/utils/BottomSheet.java
+++ app/src/main/java/com/example/jobseeker/utils/BottomSheet.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +17,12 @@ public class BottomSheet extends BottomSheetDialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.job_board_bottom_sheet,container,false);
+        View v = inflater.inflate(R.layout.job_board_bottom_sheet,container,false);
+
+        v.findViewById(R.id.close).setOnClickListener(v1 -> {
+            dismiss();
+        });
+
+        return v;
     }
 }
--- app/src/main/java/com/example/jobseeker/utils/HorizontalZoomCenterLayoutManager.java
+++ app/src/main/java/com/example/jobseeker/utils/HorizontalZoomCenterLayoutManager.java
@@ -1,6 +1,7 @@
 package com.example.jobseeker.utils;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
